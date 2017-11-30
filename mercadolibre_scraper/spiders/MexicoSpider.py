d81d408c4766a2bf2bad6543613b974372024e10
Walther Lee <walthere.lee@gmail.com>
2017-11-30 08:35:25 -0800

    pagination added to MLMexicoSpider

--- mercadolibre_scraper/spiders/MexicoSpider.py
+++ mercadolibre_scraper/spiders/MexicoSpider.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 class MLMexicoSpider(scrapy.Spider):
     name = "mercadolibre.com.mx"
-    download_delay = 5
 
     def __init__(self, name=None, **kwargs):
         super().__init__(name, **kwargs)
@@ -14,7 +13,7 @@ class MLMexicoSpider(scrapy.Spider):
         binary_string = pkgutil.get_data("mercadolibre_scraper", "resources/key_words.txt")
         self.key_words = binary_string.decode("utf-8").split("\n")
 
-        self.details_headers = {
+        self.headers = {
             "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0",
             "Accept-Language": "en",
             "Connection": "keep-alive"
@@ -26,23 +25,27 @@ class MLMexicoSpider(scrapy.Spider):
         for key_word in self.key_words:
             yield scrapy.Request(url="https://listado.mercadolibre.com.mx/" + key_word,
                                  callback=self.parse,
-                                 meta={"key_word": key_word},
-								 headers=self.details_headers)
+                                 meta={"key_word": key_word, "page": 1, "last_position": 0},
+                                 headers=self.headers)
 
     def parse(self, response):
         lis = response.selector.xpath('//ol[@id="searchResults"]/li')
-        for li in lis:
+        for position, li in enumerate(lis):
+            response.meta["last_position"] += 1
             data = {
                 "id": li.xpath('./div/@id').extract_first(),
                 "url": li.xpath('.//a/@href').extract_first(),
                 "title": li.xpath('.//h2//span/text()').extract_first().strip(),
                 "price": li.xpath('.//*[@class="price-fraction"]/text()').extract_first(),
                 "key_word": response.meta.get("key_word"),
-                "date": self.today_date
+                "date": self.today_date,
+                "position": response.meta.get("last_position"),
+                "page": response.meta.get("page")
             }
 
             yield data
 
         next_url = response.selector.xpath('//li[@class="pagination__next"]/a/@href').extract_first()
-        if next_url:
-            yield scrapy.Request(url=next_url, callback=self.parse, meta=response.meta, headers=self.details_headers)
+        if next_url and response.meta["page"] < 10:
+            response.meta["page"] += 1
+            yield scrapy.Request(url=next_url, callback=self.parse, meta=response.meta, headers=self.headers)
